4ad2aa622fe93ccd4436711ae674f971c0a24c4c
biancamacedodev <biancamcedo97@gmail.com>
2024-11-08 14:54:48 -0300

    002 - criado e estruturado componente de Screens

---
+++ mercedes-garage/src/screens/GarageScreen/GarageScreen.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+export default function GarageScreen() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.cameraBox}> 
+                <View style={styles.triangleCorner}></View>
+                <View style={[styles.triangleCorner, styles.topRight]}></View>
+                <View style={[styles.triangleCorner, styles.bottomLeft]}></View>
+                <View style={[styles.triangleCorner, styles.bottomRight]}></View>
+            </View> 
+            <Text>GarageScreen</Text>
+        </View>
+    )
+}
