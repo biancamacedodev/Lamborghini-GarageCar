ff79c3f0dc775265d1b457a38b599ac233e3a26b
biancamacedodev <biancamcedo97@gmail.com>
2024-11-08 14:55:28 -0300

    003 - criado e estruturado o componente de CardView

---
+++ mercedes-garage/src/components/CardView/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from 'react-native'
+import { styles } from "./styles";
+
+export default function CardView() {
+    return (
+        <View style={styles.imageContainer}>
+            <Text>CardView works</Text>
+        </View>
+    )
+}
